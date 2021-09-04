521424184d3686106160d6bcfc7d6f2804422135
hmtang <hmtang@gmail.com>
2021-09-04 23:47:16 +0800

    Test List & Vector

--- my-proj/src/my_proj/core.clj
+++ my-proj/src/my_proj/core.clj
@@ -1,7 +1,15 @@
 (ns my-proj.core
   (:gen-class))
+(def poemsList `("Iliad" "Odyssey" "Now We Are Six"))
+(def poemsVector ["Iliad" "Odyssey" "Now We Are Six"])
+(def newPoemsList (conj poemsList "New Item"))
 
 (defn -main
   "I don't do a whole lot ... yet."
-  [& args]
-  (print "Welcome to First Clojure Test!"))
+  []
+  (println "Hello, World!")
+  (println "List: " (conj poemsList "New Item"))
+  (println "New List: " newPoemsList)
+  (println "Vector: " (conj poemsVector "New Item"))
+  (println (nth poemsList 2))
+  (println (nth newPoemsList 3)))
